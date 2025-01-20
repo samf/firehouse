4611cb6362e9ed84b586dc4e8a588a2d2b6ac708
Sam Falkner <sam.falkner@gmail.com>
2025-01-20 12:55:57 -0700

    efmrl2 cli config
    also the old cli config

--- efmrl.config.js
+++ efmrl.config.js
@@ -1,5 +1,6 @@
 {
     "efmrl": "firehouse",
     "root_dir": "dist",
+    "base_host": "efmrl.net:8443",
     "app": {}
 }
---
+++ efmrl2.config.js
@@ -0,0 +1,8 @@
+{
+    "efmrl": "firehouse",
+    "root_dir": "dist",
+    "base_host": "slateblueweb.com",
+    "index_rewrite": [
+        "index.html"
+    ]
+}
